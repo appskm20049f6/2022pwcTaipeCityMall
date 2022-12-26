dc11d1c0d3c976a992f1d08bc8bbf6e1598a2eb5
Koi tw <appskm20049f6@gmail.com>
2022-12-26 11:41:29 +0800

    2022 12 26 1141
    背景調整

---
+++ js/app.js
@@ -0,0 +1,45 @@
+let ibendo1 = document.querySelector("#ibendo-1");
+let ibendo2 = document.querySelector("#ibendo-2");
+let ibendo3 = document.querySelector("#ibendo-3");
+let ibendo4 = document.querySelector("#ibendo-4");
+let ibendo5 = document.querySelector("#ibendo-5");
+
+let ibendoChange1 = () => {
+  ibendo1.style.display = "flex";
+  ibendo2.style.display = "none";
+  ibendo3.style.display = "none";
+  ibendo4.style.display = "none";
+  ibendo5.style.display = "none";
+};
+
+let ibendoChange2 = () => {
+  ibendo1.style.display = "none";
+  ibendo2.style.display = "flex";
+  ibendo3.style.display = "none";
+  ibendo4.style.display = "none";
+  ibendo5.style.display = "none";
+};
+
+let ibendoChange3 = () => {
+  ibendo1.style.display = "none";
+  ibendo2.style.display = "none";
+  ibendo3.style.display = "flex";
+  ibendo4.style.display = "none";
+  ibendo5.style.display = "none";
+};
+
+let ibendoChange4 = () => {
+  ibendo1.style.display = "none";
+  ibendo2.style.display = "none";
+  ibendo3.style.display = "none";
+  ibendo4.style.display = "flex";
+  ibendo5.style.display = "none";
+};
+
+let ibendoChange5 = () => {
+  ibendo1.style.display = "none";
+  ibendo2.style.display = "none";
+  ibendo3.style.display = "none";
+  ibendo4.style.display = "none";
+  ibendo5.style.display = "flex";
+};
