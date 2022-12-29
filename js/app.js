636d178b9e5ba38ce2d7282f04a83b9e49641da9
Koi tw <appskm20049f6@gmail.com>
2022-12-29 20:41:53 +0800

    2022 1229 2040
    兩個活動 兩個敬請期待

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ let ibendoChange = (c) => {
       navbar.children[index].children[0].children[4].children[0].innerHTML =
         "<p>前往</p>";
     }
-    title.innerHTML = `<img src="/img/index-title.png" alt="" data-aos="fade-down" >"`;
+    title.innerHTML = `<img src="/img/index-title.png" alt="" data-aos="fade-down">`;
 
     navbar.children[0].children[0].children[4].children[0].innerHTML =
       "<p>現在</p>";
@@ -63,4 +63,116 @@ let ibendoChange = (c) => {
     ibendo4.style.display = "flex";
     navbar.children[3].children[0].classList.add("card-change");
   }
+};
+
+// 時刻表切換
+let streamMonth = document.querySelector("#streamMonth");
+let streamToday = document.querySelector("#streamToday");
+let streamDay = document.querySelector("#streamDay");
+let vtuberTime = document.querySelector("#vtuberTime");
+let vtuberName = document.querySelector("#vtuberName");
+let vtuberPeople = document.querySelector("#vtuberPeople");
+let vtubercard = document.querySelector("#vtubercard");
+let idCard = document.querySelector("#idCard");
+let vtubercardbackground = document.querySelector("#vtubercard-background");
+let streamcard = document.querySelector("#streamcard");
+let idCardLittle = document.querySelector("#idCard-little");
+let sixup = document.querySelector(".sixup");
+
+let today = new Date();
+let month = today.getMonth() + 1;
+let kyo = today.getDate();
+
+let changeDate = (day) => {
+  axios.get("/json/data.json").then(function (response) {
+    idCard.innerHTML = "";
+    //確認今天幾個人直播並劃出卡
+    let vtuberArr = [];
+    if (day == 0) {
+      streamcard.style.display = "none";
+      for (let index = 0; index < 58; index++) {
+        idCardLittle.innerHTML += `<div class="littlecard">
+        <div class="little-head-1">
+            <div class="little-head-2">
+                <img src="${response.data[index].img}">
+            </div>
+        </div>
+        <div class="little-name">
+            <p>${response.data[index].name}</p>
+        </div>
+    </div>`;
+      }
+    } else {
+      streamcard.style.display = "block";
+      for (let index = 0; index < 58; index++) {
+        if (response.data[index].date == day) {
+          vtuberArr.push(response.data[index]);
+        }
+      }
+
+      for (let index = 0; index < vtuberArr.length; index++) {
+        idCard.innerHTML += `<a href="${vtuberArr[index].url}" class="sixup">
+  <div
+      class="vtubercard"
+      id="vtubercard"
+  >
+
+      <div class="card-1">
+          <div class="card-2">
+              <div
+                  class="vtubercard-people"
+                  id="vtuberPeople"
+              >
+              
+                  <img
+                      src=${vtuberArr[index].img}
+                  >
+
+              </div>
+              <div class="card-mark1">
+                  <div class="mark"></div>
+              </div>
+              <div class="card-mark2">
+                  <div class="mark"></div>
+              </div>
+              <div class="card-mark3">
+                  <div class="mark"></div>
+              </div>
+              <div class="card-mark4">
+                  <div class="mark"></div>
+              </div>
+          </div>
+
+      </div>
+      <div class="card-1-bottom">
+          <div class="card-1-bottom1">
+              <div class="ten-1"></div>
+          </div>
+          <div
+              class="card-1-bottom2"
+              id="vtuberName"
+          >
+              <p>${vtuberArr[index].name}</p>
+          </div>
+          <div class="card-1-bottom3">
+              <div class="ten-2"></div>
+          </div>
+      </div>
+
+      <div
+          class="vtubercard-time"
+          id="vtuberTime"
+      >
+          <p>${vtuberArr[index].time}</p>
+      </div>
+  </div>
+</a>`;
+        //修改今天日期
+        streamMonth.innerHTML = `<p>${vtuberArr[index].month}月</p>`;
+        streamDay.innerHTML = `<p>${vtuberArr[index].date}</p>`;
+      }
+    }
+    console.log(response.data);
+    console.log(vtuberArr);
+  });
 };
